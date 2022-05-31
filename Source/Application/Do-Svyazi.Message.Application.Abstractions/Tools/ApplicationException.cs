fc29ca2aa6e74f212e0bb48640b8fa18365a416c
Ksen <pks2002@yandex.ru>
2022-06-01 01:22:34 +0300

    add: default application exception

---
+++ Source/Application/Do-Svyazi.Message.Application.Abstractions/Tools/ApplicationException.cs
@@ -0,0 +1,12 @@
+﻿using Do_Svyazi.Message.Domain.Tools;
+
+namespace Do_Svyazi.Message.Application.Abstractions.Tools;
+
+public class ApplicationException : DomainException
+{
+    public ApplicationException() { }
+    
+    public ApplicationException(string? message, Exception? innerException) : base(message, innerException) { }
+    
+    public ApplicationException(string? message) : base(message) { }
+}
