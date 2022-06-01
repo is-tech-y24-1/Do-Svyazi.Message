9ce9bded00bdedcc5de18eac759ddecb2b12b0db
kutuzovmikv <kutuzovmikv@gmail.com>
2022-06-02 01:15:06 +0300

    feat: added hub and filters fix: .net can't see signalR

---
+++ Source/Presentation/Server/Do-Svyazi.Message.Server.Tcp/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Do_Svyazi.Message.Application.Dto.Chats;
+using Do_Svyazi.Message.Application.Dto.Messages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Do_Svyazi.Message.Server.Tcp.Hubs;
+
+[Authorize]
+public class ChatHub : Hub
+{
+    public async Task Enter(ChatUserDto chatUser)
+    {
+        if (string.IsNullOrEmpty(username))
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+            await Clients.Group(groupname).SendAsync("Notify", $"{username} вошел в чат");
+        }
+    }
+
+    public async Task Send(string message, string username)
+    {
+        await Clients.Group(groupname).SendAsync("Receive", message, username);
+    }
+}
