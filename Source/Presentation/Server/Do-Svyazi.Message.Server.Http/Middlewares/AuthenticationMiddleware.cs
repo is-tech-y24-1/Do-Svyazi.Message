bf4dfb933def7f01e5521c8306440c6674c7dd75
Ksen <pks2002@yandex.ru>
2022-06-02 22:31:48 +0300

    refactor: delete private mediator

--- Source/Presentation/Server/Do-Svyazi.Message.Server.Http/Middlewares/AuthenticationMiddleware.cs
+++ Source/Presentation/Server/Do-Svyazi.Message.Server.Http/Middlewares/AuthenticationMiddleware.cs
@@ -7,7 +7,6 @@ namespace Do_Svyazi.Message.Server.Http.Middlewares;
 
 public class AuthenticationMiddleware
 {
-    private IMediator _mediator;
     private readonly RequestDelegate _next;
 
     public AuthenticationMiddleware(RequestDelegate next)
@@ -17,20 +16,19 @@ public class AuthenticationMiddleware
     
     public async Task Invoke(HttpContext context, IMediator mediator)
     {
-        _mediator = mediator;
         var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
 
         if (token != null)
-            AttachUserToContext(context, token);
+            AttachUserToContext(context, token, mediator);
 
         await _next(context);
     }
 
-    private void AttachUserToContext(HttpContext context, string token)
+    private void AttachUserToContext(HttpContext context, string token, IMediator mediator)
     {
         var authenticationCredentials = new AuthenticationCredentials(token);
 
-        var response = _mediator.Send(new GetUserModel.Query(authenticationCredentials));
+        var response = mediator.Send(new GetUserModel.Query(authenticationCredentials));
 
         var user = response.Result.UserModel;
 
