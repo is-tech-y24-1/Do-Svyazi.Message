3ab88f5f5ba9dfe6aa7e6b2e4bf9d97394ac9d93
Ksen <pks2002@yandex.ru>
2022-06-02 14:33:50 +0300

    add: controller

---
+++ Source/Presentation/Server/Do-Svyazi.Message.Server.Http/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using Do_Svyazi.Message.Application.CQRS.Chats.Queries;
+using Do_Svyazi.Message.Application.Dto.Chats;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_Svyazi.Message.Server.Http.Controllers;
+
+public class MessageController : ControllerBase
+{
+    private IMediator _mediator;
+
+    public MessageController(IMediator mediator) => _mediator = mediator;
+
+    [HttpGet("GetCountOfUnreadMessages")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<int>> GetCountOfUnreadMessages(Guid userId, Guid chatId)
+    {
+        var response = await _mediator.Send(new GetChatUserState.Query(userId, chatId));
+
+        return Ok(response.ChatUserState.UnreadMessageCount);
+    }
+    
+    [HttpGet("GetChatUserState")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<ChatUserStateDto>> GetChatState(Guid userId, Guid chatId)
+    {
+        var response = await _mediator.Send(new GetChatUserState.Query(userId, chatId));
+
+        return Ok(response.ChatUserState);
+    }
+}
