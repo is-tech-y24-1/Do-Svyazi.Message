4cf76989d971258feab4711341af225d2d9c41d4
kutuzovmikv <kutuzovmikv@gmail.com>
2022-06-03 21:10:23 +0300

    fix: fixed ExceptionFilter.cs to catch exceptions properly

--- Source/Presentation/Server/Do-Svyazi.Message.Server.Tcp/Filters/ExceptionFilter.cs
+++ Source/Presentation/Server/Do-Svyazi.Message.Server.Tcp/Filters/ExceptionFilter.cs
@@ -6,43 +6,20 @@ namespace Do_Svyazi.Message.Server.Tcp.Filters;
 
 public class ExceptionFilter : IHubFilter
 {
-    public async Task InvokeMethodAsync(HubInvocationContext invocationContext, Func<HubInvocationContext, ValueTask<object?>> next)
+    public async ValueTask<object?> InvokeMethodAsync(HubInvocationContext invocationContext,
+        Func<HubInvocationContext, ValueTask<object?>> next)
     {
         try
         {
-            await next(invocationContext);
-        }
-        catch (NotFoundException e)
-        {
-            var httpContext = invocationContext.Context.GetHttpContext();
-            httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-            httpContext.Response.ContentType = "text/*";
-
-            await httpContext.Response.WriteAsync(e.Message);
-        }
-        catch (InvalidRequestException e)
-        {
-            var httpContext = invocationContext.Context.GetHttpContext();
-            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            httpContext.Response.ContentType = "text/*";
-
-            await httpContext.Response.WriteAsync(e.Message);
-        }
-        catch (UnauthorizedException e)
-        {
-            var httpContext = invocationContext.Context.GetHttpContext();
-            httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            httpContext.Response.ContentType = "text/*";
-
-            await httpContext.Response.WriteAsync(e.Message);
+            return await next.Invoke(invocationContext);
         }
         catch (Exception e)
         {
-            var httpContext = invocationContext.Context.GetHttpContext();
-            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-            httpContext.Response.ContentType = "text/*";
+            var client = invocationContext.Hub.Clients.Caller;
 
-            await httpContext.Response.WriteAsync(e.Message);
+            await client.SendAsync("ReceiveAsync", e.Message);
         }
+
+        return ValueTask.CompletedTask;
     }
 }
