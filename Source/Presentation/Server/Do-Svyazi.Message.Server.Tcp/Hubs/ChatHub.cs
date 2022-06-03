6454e45ea822b464249df23ec46243aad33f4a53
kutuzovmikv <kutuzovmikv@gmail.com>
2022-06-03 11:56:23 +0300

    fix: fixed sending type from string to MessageDto

--- Source/Presentation/Server/Do-Svyazi.Message.Server.Tcp/Hubs/ChatHub.cs
+++ Source/Presentation/Server/Do-Svyazi.Message.Server.Tcp/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Do_Svyazi.Message.Application.Dto.Messages;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Do_Svyazi.Message.Server.Tcp.Hubs;
@@ -6,6 +7,6 @@ namespace Do_Svyazi.Message.Server.Tcp.Hubs;
 [Authorize]
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
-        => await Clients.All.SendAsync("ReceiveMessage", user, message);
+    public async Task SendMessage(MessageDto messageDto)
+        => await Clients.All.SendAsync("ReceiveMessage", messageDto);
 }
