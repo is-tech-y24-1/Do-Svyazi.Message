a65800e5d5da41afb3254352692e5f29da160b34
kutuzovmikv <kutuzovmikv@gmail.com>
2022-06-11 15:18:43 +0300

    fix: used `UserIdentifier` instead of GetUserModel

--- Source/Presentation/Server/Do-Svyazi.Message.Server.Tcp/Hubs/ChatHub.cs
+++ Source/Presentation/Server/Do-Svyazi.Message.Server.Tcp/Hubs/ChatHub.cs
@@ -1,12 +1,9 @@
-﻿using Do_Svyazi.Message.Application.Abstractions.Exceptions.Unauthorized;
-using Do_Svyazi.Message.Application.Abstractions.Integrations.Models;
-using Do_Svyazi.Message.Application.CQRS.Messages.Queries;
+﻿using Do_Svyazi.Message.Application.CQRS.Messages.Queries;
 using Do_Svyazi.Message.Application.CQRS.Users.Queries;
 using Do_Svyazi.Message.Application.Dto.Messages;
 using Do_Svyazi.Message.Client.Tcp.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Do_Svyazi.Message.Server.Tcp.Hubs;
@@ -23,40 +20,34 @@ public class ChatHub : Hub<IChatClient>
 
     public override async Task OnConnectedAsync()
     {
-        var httpContext = Context.GetHttpContext();
+        var user = Context.UserIdentifier;
+        if (user != null)
+        {
+            var userId = Guid.Parse(user);
 
-        var user = GetUserModel(httpContext);
+            var query = new GetUserChatIds.Query(userId);
+            var response = await _mediator.Send(query);
 
-        var response = await _mediator.Send(new GetUserChatIds.Query(user.Id));
+            IEnumerable<Task> tasks = response.ChatIds
+                .Select(c => Groups.AddToGroupAsync(Context.ConnectionId, c.ToString()));
 
-        IEnumerable<Task> tasks = response.ChatIds
-            .Select(c => Groups.AddToGroupAsync(Context.ConnectionId, c.ToString()));
-
-        await Task.WhenAll(tasks);
+            await Task.WhenAll(tasks);
+        }
     }
 
     public async IAsyncEnumerable<MessageDto> GetMessages(Guid chatId, DateTime cursor, int count)
     {
-        var httpContext = Context.GetHttpContext();
+        var user = Context.UserIdentifier;
+        if (user == null) yield break;
 
-        var user = GetUserModel(httpContext);
+        var userId = Guid.Parse(user);
 
-        var query = new GetChatMessages.Query(user.Id, chatId, cursor, count);
-
+        var query = new GetChatMessages.Query(userId, chatId, cursor, count);
         var response = await _mediator.Send(query);
+
         foreach (var message in response.Messages)
         {
             yield return message;
         }
-    }
-
-    private static UserModel GetUserModel(HttpContext? context)
-    {
-        if (context?.Items["User"] is UserModel userModel)
-        {
-            return userModel;
-        }
-
-        throw new UnauthenticatedException();
     }
 }
